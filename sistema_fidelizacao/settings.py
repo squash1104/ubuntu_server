573ad8b7f22229cc327cf729e0653aff0b881b6d
Luciano Marchi de Souza <lucianolrv@gmail.com>
2025-07-19 07:10:54 +0000

    files

--- sistema_fidelizacao/settings.py
+++ sistema_fidelizacao/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static'), # Aponta para a pasta 'static' que criamos na raiz do projeto
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
