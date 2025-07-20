590329923cfd3b6bf5af7b084c15af87d2a4b5b3
Luciano Marchi de Souza <lucianolrv@gmail.com>
2025-07-20 09:50:09 +0000

    files

--- colaboradores/models.py
+++ colaboradores/models.py
@@ -3,11 +3,12 @@ from django.db import models
 class Colaborador(models.Model):
     nome = models.CharField(max_length=100)
     contato = models.CharField(max_length=20, blank=True, null=True)
-    cidade = models.CharField(max_length=100, blank=True, null=True)
-    bairro = models.CharField(max_length=100, blank=True, null=True)
+    cidade = models.ForeinKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
+    bairro = models.ForeinKey(Bairro, on_delete=models.SET_NULL, null=True, blank=True)
     cargo = models.CharField(max_length=100, blank=True, null=True)
  
     class Meta:
+        db_table = 'colaboradores_colaborador'
         verbose_name = "Colaborador"
         verbose_name_plural = "Colaboradores"
         ordering = ['nome']
--- convidados/models.py
+++ convidados/models.py
@@ -5,11 +5,12 @@ class Convidado(models.Model):
 	nome = models.CharField(max_length=255)
 	telefone = models.CharField(max_length=20)
 	email = models.EmailField(max_length=255, blank=True, null=True)
-	bairro = models.CharField(max_length=100, blank=True, null=True)
-	cidade = models.CharField(max_length=100, blank=True, null=True)
+    cidade = models.ForeinKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
+    bairro = models.ForeinKey(Bairro, on_delete=models.SET_NULL, null=True, blank=True)
 	colaborador = models.ForeignKey(Colaborador, on_delete=models.SET_NULL, null=True, related_name='convidados')
 
 	class Meta:
+        db_table = 'convidados_convidado'
 		verbose_name = "Convidado"
 		verbose_name_plural = "Convidados"
 		ordering = ['nome']
