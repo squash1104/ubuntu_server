8266191a3a03c9f55af07b29c006b9f37ba454d5
Luciano Marchi de Souza <lucianolrv@gmail.com>
2025-07-20 10:40:14 +0000

    files

--- colaboradores/models.py
+++ colaboradores/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from geografia.models import Cidade, Bairro
 
 class Colaborador(models.Model):
     nome = models.CharField(max_length=100)
     contato = models.CharField(max_length=20, blank=True, null=True)
-    cidade = models.ForeinKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
-    bairro = models.ForeinKey(Bairro, on_delete=models.SET_NULL, null=True, blank=True)
+    cidade = models.ForeignKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
+    bairro = models.ForeignKey(Bairro, on_delete=models.SET_NULL, null=True, blank=True)
     cargo = models.CharField(max_length=100, blank=True, null=True)
- 
+
     class Meta:
         db_table = 'colaboradores_colaborador'
         verbose_name = "Colaborador"
--- convidados/models.py
+++ convidados/models.py
@@ -1,19 +1,18 @@
 from django.db import models
-from colaboradores.models import Colaborador
+from geografia.models import Cidade, Bairro
 
 class Convidado(models.Model):
-	nome = models.CharField(max_length=255)
-	telefone = models.CharField(max_length=20)
-	email = models.EmailField(max_length=255, blank=True, null=True)
-    cidade = models.ForeinKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
-    bairro = models.ForeinKey(Bairro, on_delete=models.SET_NULL, null=True, blank=True)
-	colaborador = models.ForeignKey(Colaborador, on_delete=models.SET_NULL, null=True, related_name='convidados')
+    nome = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    cidade = models.ForeignKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
+    bairro = models.ForeignKey(Bairro, on_delete=models.SET_NULL, null=True, blank=True)
 
-	class Meta:
-        db_table = 'convidados_convidado'
-		verbose_name = "Convidado"
-		verbose_name_plural = "Convidados"
-		ordering = ['nome']
+class Meta:
+    db_table = 'convidados_convidado'
+    verbose_name = "Convidado"
+    verbose_name_plural = "Convidados"
+    ordering = ['nome']
 
-	def __str__(self):
-		return self.nome
+    def __str__(self):
+        return self.nome
