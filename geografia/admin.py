186b21a08b36cb3afdc9570fd129136d8d25ada4
Luciano Marchi de Souza <lucianolrv@gmail.com>
2025-07-27 06:38:45 +0000

    commit 26-07-2025

--- geografia/admin.py
+++ geografia/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Cidade, Bairro # 1. Importe os seus modelos
 
-# Register your models here.
+# 2. Crie classes de admin para customização (opcional, mas boa prática)
+class CidadeAdmin(admin.ModelAdmin):
+    list_display = ('nome_cidade', 'uf_cidade') # Colunas a exibir na lista
+    search_fields = ('nome_cidade', 'uf_cidade') # Adiciona uma barra de busca
+
+class BairroAdmin(admin.ModelAdmin):
+    list_display = ('nome_bairro', 'cidade') # Colunas a exibir na lista
+    search_fields = ('nome_bairro',) # Adiciona uma barra de busca
+    list_filter = ('cidade',) # Adiciona um filtro lateral por cidade
+
+# 3. Registe os seus modelos com as suas classes de admin
+admin.site.register(Cidade, CidadeAdmin)
+admin.site.register(Bairro, BairroAdmin)
